62e512dc93c7023fa9d0049b899b5a1388414ee2
Neavy18 <genevieve.gt18@gmail.com>
2022-01-06 22:45:34 -0500

    Add parameterized queries for students.js and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,31 +9,17 @@ const pool = new Pool({
 
 const month = process.argv[2]
 const max = process.argv[3]
-const values =[`${month}`, `${max}`];
+const values =[`%${month}%`, max];
 
-const query = `
-SELECT students.id, students.name,cohorts.name
+pool.query(`
+SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = $1
+WHERE cohorts.name LIKE $1
 LIMIT $2;
-`
-pool.query(query, values).then (res => {
+`, values)
+.then (res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${cohorts.name} cohort`);
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
   })
 });
-
-// --> CODE FROM THE TOGGLE <--
-/* pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack)); */
